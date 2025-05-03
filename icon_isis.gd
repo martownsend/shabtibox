9b9962d615cb5d15f7c5d2d479eaa8add51455e7
mtownsend <townsound@proton.me>
2025-05-03 14:03:50 -0700

    Implemented Isis buttons and labels.

---
+++ icon_isis.gd
@@ -0,0 +1,6 @@
+extends Sprite2D
+
+
+func _on_icon_isis_button_pressed() -> void:
+	self.visible = !self.visible
+	
---
+++ intro_isis.gd
@@ -0,0 +1,5 @@
+extends Sprite2D
+
+
+func _on_intro_button_isis_pressed() -> void:
+	self.visible = !self.visible
